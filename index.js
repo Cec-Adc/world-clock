70c65275126a14657acb397442c289057db2daec
Cec-Adc <83160858+Cec-Adc@users.noreply.github.com>
2022-11-17 18:13:01 +0100

    Added the select functionality

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //Los Angeles
 function showLosAngelesTime() {
   let losAngelesDateElement = document.querySelector("#los-angeles .date");
-  let losAngelesCurrentDate = moment().format("MMMM Do YYYY");
-  losAngelesDateElement.innerHTML = losAngelesCurrentDate;
+  if (losAngelesDateElement) {
+    let losAngelesCurrentDate = moment().format("MMMM Do YYYY");
+    losAngelesDateElement.innerHTML = losAngelesCurrentDate;
 
-  let losAngelesTimeElement = document.querySelector("#los-angeles .time");
-  let losAngelesCurrentTime = moment()
-    .tz("America/Los_Angeles")
-    .format("h:mm:ss [<small>]A[</small>]");
-  losAngelesTimeElement.innerHTML = losAngelesCurrentTime;
+    let losAngelesTimeElement = document.querySelector("#los-angeles .time");
+    let losAngelesCurrentTime = moment()
+      .tz("America/Los_Angeles")
+      .format("h:mm:ss [<small>]A[</small>]");
+    losAngelesTimeElement.innerHTML = losAngelesCurrentTime;
+  }
 }
 
 showLosAngelesTime(); //so that it shows some text on the page when it's loading, otherwise it would be empty
@@ -17,15 +19,40 @@ setInterval(showLosAngelesTime, 1000);
 //Sydney
 function showSydneyTime() {
   let sydneyDateElement = document.querySelector("#sydney .date");
-  let sydneyCurrentDate = moment().format("MMMM Do YYYY");
-  sydneyDateElement.innerHTML = sydneyCurrentDate;
+  if (sydneyDateElement) {
+    let sydneyCurrentDate = moment().format("MMMM Do YYYY");
+    sydneyDateElement.innerHTML = sydneyCurrentDate;
 
-  let sydneyTimeElement = document.querySelector("#sydney .time");
-  let sydneyCurrentTime = moment()
-    .tz("Australia/Sydney")
-    .format("h:mm:ss [<small>]A[</small>]");
-  sydneyTimeElement.innerHTML = sydneyCurrentTime;
+    let sydneyTimeElement = document.querySelector("#sydney .time");
+    let sydneyCurrentTime = moment()
+      .tz("Australia/Sydney")
+      .format("h:mm:ss [<small>]A[</small>]");
+    sydneyTimeElement.innerHTML = sydneyCurrentTime;
+  }
 }
 
 showSydneyTime(); //so that it shows some text on the page when it's loading, otherwise it would be empty
 setInterval(showSydneyTime, 1000);
+
+//Multiple locations
+function showTimeInSelectedCity(event) {
+  let selectedCityTimeZone = event.target.value;
+  let cityName = selectedCityTimeZone.replace("_", " ").split("/")[1];
+  let selectedCityData = moment().tz(selectedCityTimeZone);
+  let selectedCityElement = document.querySelector("#cities");
+  selectedCityElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${selectedCityData.format("MMMM Do YYYY")}</div>
+    </div>
+    <div>
+      <div class="time">${selectedCityData.format(
+        "h:mm:ss"
+      )} <small>${selectedCityData.format("A")}</small></div>
+    </div>
+  </div>`;
+}
+
+let selectCitiesElement = document.querySelector("#city");
+selectCitiesElement.addEventListener("change", showTimeInSelectedCity);
